895a1f1c53be270f947204a956f1fc7f0fb1ac26
repoe2e <parenteme18@hotmail.com>
2024-09-04 01:18:58 -0300

    inclusão elementos login

--- src/test/java/elementos2/ElementosWeb2.java
+++ src/test/java/elementos2/ElementosWeb2.java
@@ -3,10 +3,17 @@ package elementos2;
 import org.openqa.selenium.By;
 
 public class ElementosWeb2 {
-
+                                       //eu não gosto de fila
 	By btnLogin = By.xpath("//button.[@class='login-button'] ");
 	By username = By.xpath("//input[@name='username']");
-	//eu não gosto de fila
+	By password = By.xpath("//input[@name='password']");
+	By btnContinuar = By.xpath("//button[@class='form-button']"); 
+	By btnCancelar = By.xpath("//button[@class='form-button secondary-button']"); 
+	
+	//Elementos abrir conta
+	By btnAbrirConta = By.xpath("//button[@class='open-account-button']");
+	
+	
 	
 	
 }

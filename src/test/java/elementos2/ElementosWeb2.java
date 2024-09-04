54b0821ca81444734bb3eeac44e19263d243b824
repoe2e <parenteme18@hotmail.com>
2024-09-03 22:58:18 -0300

    inclusão do projeto

---
+++ src/test/java/elementos2/ElementosWeb2.java
@@ -0,0 +1,12 @@
+package elementos2;
+
+import org.openqa.selenium.By;
+
+public class ElementosWeb2 {
+
+	By btnLogin = By.xpath("//button.[@class='login-button'] ");
+	By username = By.xpath("//input[@name='username']");
+	
+	
+	
+}
